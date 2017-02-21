a27d085d64499ea03b334e02828d8bb36779bf92
herulor <vhcervantesb@unal.edu.co>
2017-02-21 13:04:17 -0500

    second commit

---
+++ bin/shutdown_dialog.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/sh
+
+ACTION=`zenity --width=90 --height=150 --list --text="Select logout action" --title="Logout" --column "Action" Shutdown Reboot`
+
+if [ -n "${ACTION}" ];then
+ case $ACTION in
+ Shutdown)
+   shutdown
+   ;;
+ Reboot)
+   reboot
+   ;;
+ esac
+fi
